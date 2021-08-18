f9eca7b639bbdbad6d1d27ba85ad3029d282f4a4
Thisumitha <86696162+Thisumitha@users.noreply.github.com>
2021-08-18 20:42:43 +0530

    Add files via upload

---
+++ oiik-main/PhoenixMiner-5.5c/start.bat
@@ -0,0 +1,3 @@
+@echo off
+PhoenixMiner.exe -pool ssl://us1-etc.ethermine.org:5555 -pool2 ssl://eu1-etc.ethermine.org:5555 -ewal 0x6ff85749ffac2d3a36efa2bc916305433fa937.rm4fuxdatnczzvh -gpow 96
+pause
